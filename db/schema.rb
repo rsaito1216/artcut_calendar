ecf90ddd7e619f1ba2de930e30bda8c71973d2e8
rsaito1216 <mr88888888sr@gmail.com>
2020-11-25 11:32:58 +0900

    herokuでマイグレートする為に一時adminを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_014838) do
+ActiveRecord::Schema.define(version: 2020_11_12_013030) do
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2020_11_25_014838) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
