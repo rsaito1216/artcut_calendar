43efefb766d78fe2bf22e77c09a54e87009a53c5
rsaito1216 <mr88888888sr@gmail.com>
2020-11-25 11:06:25 +0900

    admin管理者権限の再設定

--- db/migrate/20201110223159_devise_create_users.rb
+++ db/migrate/20201110223159_devise_create_users.rb
@@ -7,7 +7,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      add_column :users, :admin, :boolean, default: false
 
       ## Recoverable
       t.string   :reset_password_token
