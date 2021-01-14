964ac30dd00902cabfe479431deb6a0b2dddeb2e
rsaito1216 <mr88888888sr@gmail.com>
2021-01-14 17:46:49 +0900

    ユーザー編集、ロゴ調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_account_update_parameters, if: :devise_controller?
   # before_action :basic_auth
 
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  def configure_account_update_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
   end
 
   def basic_auth
